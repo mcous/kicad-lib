7e18100f5afe6a589edbb6238cdb67be4510295d
Michael Cousins <cousmj@gmail.com>
2014-02-27 15:01:59 -0600

    beginning migration from legacy footprints to s-expr directories

---
+++ kicad.sh
@@ -0,0 +1,6 @@
+# kicad exports
+# in linux, file should be linked to /etc/profile.d/kicad.sh
+# mc 2/27/2014
+
+export KIGITHUB=https://github.com/KiCad
+export KISYSMOD=${HOME}/projects/kicad-lib/footprints
